b19db906e0134430dbda8cbdc64918bad51065a7
Dmytro Kharchenko <Kharchenko_D@island-ukraine.com>
2017-11-16 11:50:25 +0200

    edit links

--- src/Conponents/Menu.js
+++ src/Conponents/Menu.js
@@ -1,23 +1,48 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
-import '../css/header.css'
+import { NavLink, Link, BrowserRouter, Route} from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+import '../css/header.css';
+
+const history = createBrowserHistory();
+
+const Home = () => (
+    <div>
+        <h2>Головна</h2>
+    </div>
+);
+const Products = () => (
+    <div>
+        <h2>Продукти 1С</h2>
+    </div>
+);
+const Video = () => (
+    <div>
+        <h2>Відеоспостереження</h2>
+    </div>
+);
+
+
 class Menu extends Component {
     render() {
         return (
-            <div className='header-wrap'>
-                <div className="header">
-                    <div className='logo'>
-                        <Link to='/#'><img src={require('../img/logo-island.png')} className='logo__item' alt="Логотип"/></Link>
+            <BrowserRouter history={history}>
+                <div className='header-wrap'>
+                    <div className="header">
+                        <div className='logo'>
+                            <Link to='/'><img src={require('../img/logo-island.png')} className='logo__item' alt="Логотип"/></Link>
+                        </div>
+                        <nav className='top-menu'>
+                            <NavLink to='/' className='menu__item'>Головна</NavLink>
+                            <NavLink to='/products' className='menu__item'>Продукти 1С</NavLink>
+                            <NavLink to='/video' className='menu__item'>Відеоспостереження</NavLink>
+                        </nav>
+                            <Route exact path="/" component={Home}/>
+                            <Route path="/products" component={Products}/>
+                            <Route path="/video" component={Video}/>
                     </div>
-                    <nav className='top-menu'>
-                        <NavLink to='/#' className='menu__item'>Головна</NavLink>
-                        <NavLink to='/products' className='menu__item'>Продукти 1С</NavLink>
-                        <NavLink to='/video' className='menu__item'>Відеоспостереження</NavLink>
-                    </nav>
                 </div>
-
-            </div>
+            </BrowserRouter>
         );
     }
 }
